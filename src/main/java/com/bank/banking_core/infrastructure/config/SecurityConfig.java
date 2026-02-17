c88b045126afb1e0edbfcbd51244ec8ff60aff5c
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2026-02-17 09:39:51 -0300

    Temporarily disable CSRF protection for architectural testing.

---
+++ src/main/java/com/bank/banking_core/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.bank.banking_core.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(
+                auth -> auth.requestMatchers("/accounts/**")
+                            .permitAll()
+                            .anyRequest()
+                            .authenticated()
+            )
+            .httpBasic(basic -> basic.disable())
+            .formLogin(form -> form.disable());
+
+        return http.build();
+    }
+}
